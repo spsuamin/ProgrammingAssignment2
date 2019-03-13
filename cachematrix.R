5f9c32a24a4d47a8d8fce88a6c35f37e721f1b74
spsuamin <spsuamin@gmail.com>
2019-03-12 23:36:37 -0400

    baseline code
    baseline code for makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  These two functions together calculate the inverse matrix given input argument X and store result in cache to avoid recalculating.
 
-## Write a short comment describing this function
+## makeCacheMatrix returns list of setter and getter functions required for caching the original matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        #
+        setMatrix <-function(thematrix){
+                x <<- thematrix
+                m <<- NULL
+        }
+        #
+        getMatrix <- function() x
+        #
+        setInverseMatrix <- function(calcInvMatrix) m <<- calcInvMatrix
+        #
+        getInverseMatrix <- function() m
+        
+        #
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the InverseMatrix in memory if it exists, otherwise calculates inverse matrix for X and then stores in cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverseMatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        matrixData <- x$getMatrix()
+        m <-solve(matrixData)
+        x$setInverseMatrix(m)
+        m
+        
 }
